d1547c22e4fe5e41b16f69652605f8a6bc6590bf
Шевченко Илья <shefchenkornd@gmail.com>
2018-04-03 09:01:01 +0300

    Services

---
+++ www/wp-content/themes/aletheme/archive-services.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<h2>Services</h2>
+
+<div>
+    <?php echo get_breadcrumbs(); ?>
+</div>
+
+<div>
+    <?php echo ale_get_option('service_desc'); ?>
+</div>
+
+
+<?php get_footer(); ?>
