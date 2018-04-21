8ca2ac7bfa47b7409f3146ac928009d810599cfb
Шевченко Илья <shefchenkornd@gmail.com>
2018-04-21 23:53:14 +0300

    Добавил правки после переноса сайта на хостинг

--- www/wp-content/themes/aletheme/js/custom.js
+++ www/wp-content/themes/aletheme/js/custom.js
@@ -190,9 +190,27 @@ jQuery(document).ready(function(){
                 }
             }
         });
-
-
     }
+
+
+    if (jQuery('.products_list').length > 0)
+    {
+        jQuery('.products_list').on('click', '.order_catalog', function (e) {
+            let target = jQuery(this).parents('.item-title').find('.item_data').text();
+            // console.log(target)
+            jQuery('input[name=material]').val(target);
+        });
+
+        // очищаем поле 'Материал / Услуга*' в pop-up 'Заказать'
+        jQuery("[data-fancybox]").fancybox({
+            'afterClose': function() {
+                if (jQuery('input[name=material]').length > 0){
+                    jQuery('input[name=material]').val('');
+                }
+            }
+        });
+    }
+
 
     if (jQuery('#topMenu').length > 0)
     {
